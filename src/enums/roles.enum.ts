b9cbefdd33f6d04b17c7c31b49f470677253015b
Boburbek Kosimov <qosimov_boburbek@tuneconsulting.net>
2024-08-15 10:18:01 +0500

    fx

--- src/enums/roles.enum.ts
+++ src/enums/roles.enum.ts
@@ -1,15 +1,15 @@
 export enum AccessRoles {
-  ADMIN = 1,
-  OPERATOR = 2,
-  DOCUMENT = 3,
+  SUPER_ADMIN = 1,
+  ADMIN = 2,
+  GET_PERSONAL_INFO = 3,
   ID_CARD = 4,
   TEMPORARY_PERMAMENT_REGISTRATION = 5,
 }
 
 export enum Roles {
+  SUPER_ADMIN = 'super_admin',
   ADMIN = 'admin',
-  OPERATOR = 'operator',
-  DOCUMENT = 'document',
+  GET_PERSONAL_INFO = 'get_personal_info',
   ID_CARD = 'id_card',
   TEMPORARY_PERMAMENT_REGISTRATION = 'temporary_permission_registration',
 }
